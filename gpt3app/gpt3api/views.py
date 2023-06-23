44c96cbf4075bbb0d261d7e96b063b22ff9fcb5a
KarryMorphy <93188900+stackByStack@users.noreply.github.com>
2023-06-23 22:53:07 +0800

    Update views.py

--- gpt3app/gpt3api/views.py
+++ gpt3app/gpt3api/views.py
@@ -10,20 +10,21 @@ import openai
 
 
 @csrf_exempt
-def gpt3_request(request): 
-    if request.method == 'POST' :
-        prompt = request.POST.get('prompt', '')
+def gpt3_request(request):
+    if request.method == 'GET' :
+        prompt = request.GET.get('prompt', '')
+        prompt = prompt.encode('ascii', 'ignore').decode('ascii')
         # Call the OpenAI GPT-3 API
         # Replace 'YOUR_API_KEY' with your actual OpenAI API key
         openai.api_key = os.environ.get('API_KEY')
         response = openai.Completion.create(
-            engine='gpt-3.5-turbo',
+            engine='text-davinci-003',
             prompt=prompt,
-            max_tokens=1024,
+            max_tokens=128,
             n = 1,
             stop=None,
             temperature=0.7,
         )
 
-        return JsonResponse({'result': response.choices[0].text})
+        return JsonResponse({'prompt' : prompt, 'result': response.choices[0].text})
     return JsonResponse({'error': 'Invalid request method'})
